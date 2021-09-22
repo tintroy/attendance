95030195a74b2397ff17e54c8f5bbfa996e602bc
tintroy <ashfalahnodore28@gmail.com>
2021-09-22 21:11:27 +0800

    Update edit.php

--- edit.php
+++ edit.php
@@ -53,7 +53,8 @@ else {
         <small id="phoneHelp" class="form-text text-muted">We'll never share your phone number with anyone else.</small>
         </div>
 
-    <button type="submit" name="submit" class="btn btn-success btn-block">Save Changes</button>
+    <a href="viewrecords.php" class="btn btn-info">Back To List</a>
+    <button type="submit" name="submit" class="btn btn-success">Save Changes</button>
 </form>
 <?php } ?>
 <br>
